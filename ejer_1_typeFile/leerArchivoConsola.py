d493939b026ff2658354a6f69070025057f201ee
María <mchaparrocaballero@gmail.com>
2025-10-07 20:03:18 +0200

    tabulamos el ejercicio typeFile y lo comentamos, y del findFile manejamos el control de 0 coincidencias y comentario y tabulaciones

--- ejer_1_typeFile/leerArchivoConsola.py
+++ ejer_1_typeFile/leerArchivoConsola.py
@@ -1,21 +1,33 @@
 import os
 import sys
 
-#los argumentos que le pasamos por consola se guardan en sys.argv, siendo el primer elemento el nombre del archivo
+#los argumentos que le pasamos por consola se guardan en sys.argv, 
+# siendo el de la posicion 0 el nombre del archivo del programa 
+# y el de la posicion 1 seria ya el nombre del archivo que le pasamos 
 archivo = sys.argv[1]
 
 
-#LIMPIAR PANTALLA en windows necesitas cls para borrar la pantalla, y clear para linux, por eso debemos especificar que sistema operativo es con os y dependiendo de lo que nos devuelva poner uno o otro
+#LIMPIAR PANTALLA en windows necesitas cls para borrar la pantalla, y clear para linux, 
+# por eso debemos especificar que sistema operativo es con os y dependiendo de lo que nos devuelva poner uno o otro
+#tecnicamente no hace falta limpiar la pantalla pero lo hace menos confuso
 os.system('cls' if os.name == 'nt' else 'clear')
 
 
-#dentro de un try (para controlar errores por si no encuentra el archivo o x cosas) abrimos el archivo en modo lectura
-#con encoding="utf-8" nos aseguramos que si el archivo tiene caracteres especiales no de error, después lo recorremos con un for in pero le pedimos que tambien nos escupa el indice de la linea por la que va (al leerlo linea por linea no necesitamos .read)
+#dentro de un try (para controlar errores por si no encuentra el archivo o x cosas) 
+#abrimos el archivo en modo lectura
+#con encoding="utf-8" nos aseguramos que si el archivo tiene caracteres especiales no de error,
+#después lo recorremos con un for in pero le pedimos que tambien nos escupa el indice de la linea
+#por la que va (al leerlo linea por linea no necesitamos .read)
 try:
     with open(archivo, 'r', encoding="utf-8") as file:
         for indice,line in enumerate(file):
-            #el end='' es para que no nos meta un salto de linea extra al imprimir y el +1 es para que el indice empiece en 1 en vez de 0
+
+            #el end='' es para que no nos meta un salto de linea extra al imprimir 
+            # (porque tambien imprime los saltos de linea del propio archivo)
+            #y el +1 es para que el indice empiece en 1 en vez de 0
+            
             print(indice+1,line, end='')
+
 except FileNotFoundError:
     print(f"El archivo '{archivo}' no existe")
 except Exception as e:
--- ejer_2_findFile/FindFile.py
+++ ejer_2_findFile/FindFile.py
@@ -1,23 +1,32 @@
 import os
 import sys
 
-#los argumentos que le pasamos por consola se guardan en sys.argv, siendo el primer elemento el programa (posicion 0), el segundo el archivo donde buscar (posición 1) y el tercero la palabra a buscar (posición 2)
+#los argumentos que le pasamos por consola se guardan en sys.argv,
+#  siendo el primer elemento el programa (posicion 0), el segundo el archivo donde buscar (posición 1) y
+#  el tercero la palabra a buscar (posición 2)
 archivo = sys.argv[1]
 palabra = sys.argv[2]
 
 
-#LIMPIAR PANTALLA en windows necesitas cls para borrar la pantalla, y clear para linux, por eso debemos especificar que sistema operativo es con os y dependiendo de lo que nos devuelva poner uno o otro
+#LIMPIAR PANTALLA en windows necesitas cls para borrar la pantalla,
+#  y clear para linux, por eso debemos especificar que sistema operativo es con os 
+# y dependiendo de lo que nos devuelva poner uno o otro
 os.system('cls' if os.name == 'nt' else 'clear')
 try:
     #declaramos variable contador para contar las veces que aparece la palabra empieza en 0
     contador = 0
     with open(archivo, 'r', encoding="utf-8") as file:
-         #leemos el archivo linea a linea y por cada linea contamos cuantas veces aparece la palabra con .count y se lo sumamos al contador, no remplazamos sino que sumamos.
+         #leemos el archivo linea a linea y por cada linea contamos cuantas veces aparece la palabra
+         #  con .count y se lo sumamos al contador, no remplazamos sino que sumamos.
          for linea in (file):
-               contador +=linea.count(palabra)
-    #imprimimos el resultado con la palabra y el archivo donde la hemos buscado y el contador con las veces que ha aparecido#
-    print(f"{palabra}:{contador}")
-         
+            contador +=linea.count(palabra)
+    #imprimimos el resultado con la palabra y el archivo donde la hemos buscado y 
+    # el contador con las veces que ha aparecido#
+    if contador>0 :
+        print(f"{palabra}:{contador}") 
+    else:
+        print("no se han encontrado instancias de ese caracter") 
+          
 except FileNotFoundError:
     print(f"El archivo '{archivo}' no existe")
 except Exception as e:
