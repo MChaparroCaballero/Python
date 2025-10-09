e4eed1ecf43fb132ccf15066d2db077d99115066
Maria Chaparro <mchaparrocaballero@gmail.com>
2025-10-09 14:08:23 +0200

    ejer 1 falta de argumentos

--- ejer_1_typeFile/leerArchivoConsola.py
+++ ejer_1_typeFile/leerArchivoConsola.py
@@ -3,6 +3,11 @@
 import os
 import sys
 
+
+if len(sys.argv) != 2:
+    print("No me has pasado todos los argumentos, necesito nombre del programa y el archivo a leer")
+    sys.exit(1)
+
 #los argumentos que le pasamos por consola se guardan en sys.argv, 
 # siendo el de la posicion 0 el nombre del archivo del programa 
 # y el de la posicion 1 seria ya el nombre del archivo que le pasamos 
