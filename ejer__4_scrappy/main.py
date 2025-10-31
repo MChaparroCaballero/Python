beba55c711922050272578a6aaf68060568bcb05
MARÍA CHAPARRO CABALLERO <mchaparrocaballero@gmail.com>
2025-10-31 01:39:46 +0100

    EMPEZAMOS SCRAPPY

---
+++ ejer__4_scrappy/main.py
@@ -0,0 +1,5 @@
+import requests  # Librería para hacer peticiones HTTP
+from bs4 import BeautifulSoup  # Librería para analizar HTML
+from bs4.element import Tag  # Tipo para elementos HTML
+
+url= "http://127.0.0.1:5500/ejer__4_scrappy/periodicos.html"
